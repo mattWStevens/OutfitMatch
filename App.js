24a3c16d10dbc1b310fb7f0c6be666ae08074568
mattWStevens <gibsonhistoric@gmail.com>
2024-08-03 22:03:49 -0600

    Animating button press for Button.js with react-native gesture handler and reanimated.

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 // Library Imports
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 // Screen Imports
 import { HomeScreen } from './screens/HomeScreen';
@@ -10,23 +11,25 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name='Home'
-          component={HomeScreen}
-          options={{
-            headerShown: false
-          }}
-        />
-        <Stack.Screen
-          name='PhotoSubmission'
-          component={PhotoSubmissionScreen}
-          options={{
-            title: 'Submit Photo'
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <GestureHandlerRootView>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name='Home'
+            component={HomeScreen}
+            options={{
+              headerShown: false
+            }}
+          />
+          <Stack.Screen
+            name='PhotoSubmission'
+            component={PhotoSubmissionScreen}
+            options={{
+              title: 'Submit Photo'
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
--- components/Button.js
+++ components/Button.js
@@ -1,5 +1,14 @@
 // Library Imports
-import { StyleSheet, View, Pressable, Text } from 'react-native';
+import { StyleSheet, Pressable, Text } from 'react-native';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import Animated,
+{
+    useAnimatedStyle,
+    useSharedValue,
+    withTiming,
+    Easing,
+    ReduceMotion
+} from 'react-native-reanimated';
 
 // Utility Imports
 import { theme } from '../utilities/theme';
@@ -10,17 +19,52 @@ export default function Button({
     labelStyles,
     onPress
 }) {
-    // TODO: Add logic to animate button on tap.
+    const opacity = useSharedValue(1);
+    const tap = Gesture.Tap()
+        .onTouchesDown(() => {
+            opacity.value = .2;
+        })
+        .onTouchesUp(() => {
+            opacity.value = 1;
+        })
+        .onTouchesMove(() => {
+            opacity.value = .2;
+        })
+        .onTouchesCancelled(() => {
+            opacity.value = 1;
+        });
+    const animatedOpacityStyle = useAnimatedStyle(() => {
+        return {
+            opacity: withTiming(opacity.value, {
+                duration: 150,
+                easing: Easing.bezier(0.25, 0.1, 0.25, 1),
+                reduceMotion: ReduceMotion.System
+            })
+        };
+    });
 
     return (
-        <View style={[styles.buttonContainer, { ...buttonStyles }]}>
-            <Pressable style={styles.button} onPress={onPress}>
-                <Text
-                    style={[styles.buttonLabel, { ...labelStyles }]}>
-                    {label}
-                </Text>
-            </Pressable>
-        </View>
+        <GestureDetector gesture={tap}>
+            <Animated.View
+                style={[
+                    styles.buttonContainer,
+                    animatedOpacityStyle,
+                    { ...buttonStyles }
+                ]}>
+                <Pressable
+                    style={styles.button}
+                    onPress={onPress}
+                >
+                    <Text
+                        style={[
+                            styles.buttonLabel,
+                            { ...labelStyles }
+                        ]}>
+                        {label}
+                    </Text>
+                </Pressable>
+            </Animated.View>
+        </GestureDetector>
     );
 }
 
